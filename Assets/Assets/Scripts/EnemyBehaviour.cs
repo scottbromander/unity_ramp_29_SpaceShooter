dabf45687aba747badc7ac5d2ef92415e4f0be6f
Scott Bromander <scott@primeacademy.io>
2016-07-03 22:20:29 -0500

    add enemy and laser collision

--- Assets/Assets/Scripts/EnemyBehaviour.cs
+++ Assets/Assets/Scripts/EnemyBehaviour.cs
@@ -5,16 +5,20 @@ public class EnemyBehaviour : MonoBehaviour {
 	public int health = 2;
 
 	void OnCollisionEnter2D(Collision2D theCollision){
-		Debug.Log ("Hit: " + theCollision.gameObject.name);
+		
 
 		if (theCollision.gameObject.name.Contains("laser")) {
 			LaserBehaviour laser = theCollision.gameObject.GetComponent ("LaserBehaviour") as LaserBehaviour;
 			health -= laser.damage;
+			print ("Health: " + health);
 			Destroy (theCollision.gameObject);
 		}
 
 		if (health <= 0) {
+			print ("Dead");
 			Destroy (this.gameObject);
+			GameController controller = GameObject.FindGameObjectWithTag ("GameController").GetComponent<GameController> ();
+			controller.KilledEnemy ();
 		}
 	}
 
---
+++ Assets/Assets/Scripts/GameController.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+	public Transform enemy;
+
+	[Header("Wave Properties")]
+	public float timeBeforeSpawning = 1.5f;
+	public float timeBetweenEnemies = 0.25f;
+	public float timeBeforeWaves = 2.0f;
+
+	public int enemiesPerWave = 10;
+	private int currentNumberOfEnemies = 0;
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine(SpawnEnemies());
+	}
+
+	IEnumerator SpawnEnemies() {
+		yield return new WaitForSeconds (timeBeforeSpawning);
+
+		while (true) {
+			if (currentNumberOfEnemies <= 0) {
+				for (int i = 0; i < enemiesPerWave; i++) {
+					float randDistance = Random.Range (10, 25);
+
+					Vector2 randDirection = Random.insideUnitCircle;
+					Vector3 enemyPos = this.transform.position;
+
+					enemyPos.x = randDirection.x * randDistance;
+					enemyPos.y = randDirection.y * randDistance;
+
+					Instantiate (enemy, enemyPos, this.transform.rotation);
+					currentNumberOfEnemies++;
+					yield return new WaitForSeconds (timeBetweenEnemies);
+				}
+			}
+
+			yield return new WaitForSeconds (timeBeforeWaves);
+		}
+	}
+
+	public void KilledEnemy(){
+		currentNumberOfEnemies--;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
